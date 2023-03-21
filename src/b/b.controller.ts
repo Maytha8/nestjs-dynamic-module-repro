998bb5b6a3e51f3f9c7f75c5c6c834fbf323e099
Maytham <44939173+Maytha8@users.noreply.github.com>
2023-03-21 12:14:00 +0800

    remove subpath from controller

--- src/b/b.controller.ts
+++ src/b/b.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BService } from './b.service';
 
-@Controller('b')
+@Controller()
 export class BController {
   constructor(private bService: BService) {}
 
